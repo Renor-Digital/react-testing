a2e744b9c4f1a63a094387323eecddfb6e7e9407
Renor-Digital <83707421+Renor-Digital@users.noreply.github.com>
2021-08-31 18:20:42 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import {Cardlist} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+
+    monsters: [],
+    searchField: ''
+    };
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users') 
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+    }
+
+   render() {
+     const {monsters, searchField } = this.state;
+     const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+     )
+  return (
+        <div className='App'>
+        
+        <SearchBox 
+          placeholder='zoek monsters'
+          handleChange={
+            e => 
+              this.setState({ searchField: e.target.value })
+            }
+        />
+
+
+       
+       
+        
+        <Cardlist monsters={filteredMonsters}> 
+       </Cardlist>
+
+       
+          
+          </div>
+);
+   }
+}
+
+export default App;
