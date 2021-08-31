babbf50d3eca11a093cd6601703a64b6b599d4ef
Renor-Digital <83707421+Renor-Digital@users.noreply.github.com>
2021-08-31 18:24:54 +0200

    kleine wijziging

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         <div className='App'>
         
         <SearchBox 
-          placeholder='zoek monsters'
+          placeholder='zoek monsters of niet'
           handleChange={
             e => 
               this.setState({ searchField: e.target.value })
