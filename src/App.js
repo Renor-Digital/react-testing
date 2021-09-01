30c00e5c396666cc38739f555bba03a212b439af
Renor-Digital <83707421+Renor-Digital@users.noreply.github.com>
2021-09-01 09:47:05 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
      )
   return (
         <div className='App'>
+        <h1>Rolodex</h1>
         
         <SearchBox 
           placeholder='zoek monsters of niet'
